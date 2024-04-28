ee4cdca996084bbd694841f62e417c66e608c219
Joakim Cars <joakim.cars@palettesoftware.com>
2024-04-29 00:11:49 +0200

    wip

--- _news/uppföljningullinge.md
+++ _news/uppföljningullinge.md
@@ -1,13 +1,6 @@
 ---
 title: "UPPFÖLJNING EFTER SLÄKTMÖTET I ULLINGE AUGUSTI 2022"
 text: "Sedan mötet i Ullinge 2022 har styrelsen - i samverkan med valberedningen och flera andra släktningar – arbetat med några frågor som stod på dagordningen eller dök upp under den ”brainstorming” som ingick i mötet.
- 
-Behov av nya medlemmar i styrelsen
-I samband med släktmötet 2022 gjorde valberedningen - Hans Christian Cars och Henrik Cars - ett gediget arbete med att försöka rekrytera nya och yngre släktingar till styrelsen. Detta arbete lyckades tyvärr inte alls och den sittande styrelsen valdes om i sin helhet. 
-Nuvarande styrelse: Björn Cars, Håkan Cars, Helena Sandermark och Tinna Cars-Björling. 
-Inför kommande medlemsmöte hoppas vi nu att några yngre förmågor träder fram och anmäler sitt intresse och därmed får möjlighet att lära känna släkten ännu bättre och dessutom ha väldigt trevligt!
-Björn och Helena har sagt sig vara beredda att vara kvar i styrelsen medan Håkan och Tinna har beslutat sig för att kliva av uppdraget.
-Hugade spekulanter kan med fördel höra av sig till Henrik Cars (Henrik Cars <henrikcars@hotmail.com>)!
 "
 imagepath: "/assets/news/släktträff1920.jpg"
 authorimagepath: "/assets/authors/henrik.jpg"
--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -48,28 +48,31 @@ export default function HistoryPage() {
               Också som vuxna umgicks man och träffades familjevis så mycket som
               möjligt även om man var spridda över landet. Även barnen var ofta
               med och lärde på så vis känna sina kusiner. Som mest fanns det 20
-              kusiner. Den första bland syskonen som gick bort var Sally, vid
-              blott 36 års ålder, 1926. På 40-talet dog ytterligare fem syskon:
-              Thorsten, Einar, Yngve, Henning och Gunnar. Edith dog 1963, Hadar
-              1967 och Thyra 1971. De som höll ihop den allt större släkten var
-              sedan framförallt det sista syskonet Eira och den näst äldsta
-              kusinen Inga-Lisa Söderström (dotter till Edith). Kusinerna
-              träffades bl.a. hos Eira i Mariefred och hos Inga-Lisa i
-              Sollentuna. Sista gången Eira själv var med på en stor
-              släktsamling var på hennes 90-årsdag den 10 juli 1984. Hon bjöd då
-              hela släkten på middag på Edsbergs slott vid Edsvikens norra ände
-              i Sollentuna. Fyra år senare träffades släkten på Eiras begravning
-              den 27 maj 1988. Man konstaterade då att man trivdes tillsammans
-              och gärna ville fortsätta umgänget. Men man märkte också att
-              släkten hade blivit så stor att det inte längre var möjligt för
-              någon enskild släkting att samla alla. Man började därför redan
-              under begravningen att tala om att bilda en släktförening för att
-              kunna träffas regelbundet. Några släktingar började därför sätta
-              ihop ett utkast till stadgar och planera det första släktmötet som
-              hölls den 27 augusti 1989 i Läkarförbundets lokaler på Villagatan
-              i Stockholm. 73 deltagare fattade då det formella beslutet att
-              bilda Släktföreningen Cars och att anta stadgar. Till föreningens
-              förste ordförande valdes Christer Cars (son till Einar)och till
+              kusiner.
+              <br />
+              <br />
+              Den första bland syskonen som gick bort var Sally, vid blott 36
+              års ålder, 1926. På 40-talet dog ytterligare fem syskon: Thorsten,
+              Einar, Yngve, Henning och Gunnar. Edith dog 1963, Hadar 1967 och
+              Thyra 1971. De som höll ihop den allt större släkten var sedan
+              framförallt det sista syskonet Eira och den näst äldsta kusinen
+              Inga-Lisa Söderström (dotter till Edith). Kusinerna träffades
+              bl.a. hos Eira i Mariefred och hos Inga-Lisa i Sollentuna. Sista
+              gången Eira själv var med på en stor släktsamling var på hennes
+              90-årsdag den 10 juli 1984. Hon bjöd då hela släkten på middag på
+              Edsbergs slott vid Edsvikens norra ände i Sollentuna. Fyra år
+              senare träffades släkten på Eiras begravning den 27 maj 1988. Man
+              konstaterade då att man trivdes tillsammans och gärna ville
+              fortsätta umgänget. Men man märkte också att släkten hade blivit
+              så stor att det inte längre var möjligt för någon enskild släkting
+              att samla alla. Man började därför redan under begravningen att
+              tala om att bilda en släktförening för att kunna träffas
+              regelbundet. Några släktingar började därför sätta ihop ett utkast
+              till stadgar och planera det första släktmötet som hölls den 27
+              augusti 1989 i Läkarförbundets lokaler på Villagatan i Stockholm.
+              73 deltagare fattade då det formella beslutet att bilda
+              Släktföreningen Cars och att anta stadgar. Till föreningens förste
+              ordförande valdes Christer Cars (son till Einar)och till
               styrelseledamöter Hadar Cars, Gunilla Erfelt, Inga-Lisa
               Söderström, Gunnar Fahlberg, Johan Kalén och Ejda Wesslén. I
               stadgarna föreskrevs att ordinarie släktmöte med val av styrelse
@@ -86,101 +89,32 @@ export default function HistoryPage() {
               vattenfestivalerna. Det var nu dags att byta ordförande i
               föreningen och till Christers efterträdare valdes hans son Göran
               Cars. (Övriga ledamöter framgår av bifogade företeckning över
-              släktföreningens styrelser.) Det femte släktmötet hölls i
-              Linköping den 19-20 augusti 1995. Flera av bröderna i den
-              ursprungliga syskonskaran hade gått i skola i Linköping, två av
-              dem – Gunnar och Thorsten – återvände dit i mogen ålder med sina
-              familjer och en hel del i den följande generationen har också gått
-              i skola där. Det var därför naturligt att förlägga ett släktmöte
-              till Linköping. Middagen avåts i den vackra Mjellerumsgården i
-              Gamla Linköping. Till ny föreningsordförande efter Göran Cars
-              valdes Thorsten Cars j:r (son till Thorsten). Detta år beslöts
-              också att utse en arkivarie för att ta hand om intressanta
-              dokument och bilder. Till arkivarie utsågs Kerstin Kalén. År 1996
-              hölls inte något ordinarie släktmöte men en hel del släktingar
-              samlades ändå – på inbjudan av föreningens ordförande - den 20
-              april i Wrangelska palatset på Riddarholmen i Stockholm, där Svea
-              hovrätt huserar sedan 250 år. Efter rundvandring i palatset, då
-              det fanns möjlighet att utan risk för tråkiga konsekvenser pröva
-              inte bara domarsätet och åklagarens plats utan även vittnesbåset
-              och de anklagades bänk, avåts lunch i hovrättens lunchrum i
-              källaren till det äldsta rundtornet, intill den fängelsehåla där
-              kungamördaren Anckarström hölls fången. Det sjätte släktmötet
-              hölls på Utö i Stockholms skärgård den 16-17 augusti 1997 med 50
-              deltagare. Deltagarna fick bl.a. gå en tipspromenad med frågor om
-              släkten. Mötet hölls i Societetshuset medan middagen avåts på Utö
-              Värdshus. Den 7-8 augusti 1999 gick det sjunde släktmötet av
-              stapeln på Sunnersta herrgård i Uppsala med 47 deltagare. Mellan
-              de ordinarie släktmötena ordnades den 19 augusti 2000 en
-              släktträff i Upplands Väsby norr om Stockholm med besök på
-              Rosersbergs slott och i Hammarby kyrka. På dess kyrkogård besöktes
-              den äldstes av de ursprungliga kusinerna Tore Sandermarks och hans
-              hustru Gunnels grav. Det åttonde släktmötet – med 53 deltagare -
-              hölls den 25-26 augusti 2001 i Källvik i Smålands norra skärgård,
-              en brunnsidyll där den äldste i den ursprungliga syskonskaran ,
-              Gunnar, var brunnsläkare på somrarna under 1920- och 30-talen.
-              Till hans minne har den gata där han bodde döpts till ”Dr Cars
-              väg”. Vid den gatuskylten fotograferades det halvdussin deltagare
-              som hade en doktorstitel. Den 23-24 augusti 2003 hölls det nionde
-              släktmötet i Mariefred, där systrarna Thyra och Eira hade bott i
-              många år och gästfritt tagit emot sina släktingar. Thorsten
-              överlämnade ordförandeklubban till sin syster Ejda Cars Wesslén.
-              Det tionde släktmötet gick av stapeln den 20-21 augusti 2005 i
-              Mariannelund i norra Småland, i närheten av den församling där en
-              av de ursprungliga syskonen, Thorsten, varit kyrkoherde på
-              1920-talet och början av 1930-talet. Till ny ordförande efter Ejda
-              Cars Wesslén valdes Hadar Cars j:r (son till Hadar). Det elfte
-              släktmötet hölls den 25 augusti 2007 i Stockholm på inbjudan av
-              föreningens ordförande. Efter mötesförhandlingarna hemma hos Hadar
-              och hans hustru Jane visade Hadar stadshuset och middagen intogs i
-              Stadshuskällaren. Den 22-23 augusti 2009 ägde det tolfte
-              släktmötet rum på Utö i Stockholms skärgård; det var andra gången
-              ett släktmöte hölls där. En underbart vacker konsert hölls i Utö
-              kyrka med musik och sång av både små och stora medlemmar. Därefter
-              serverades middag i den närliggande skolan. Det trettonde
-              släktmötet hölls den 20-21 augusti 2011 i Medevi Brunn norr om
-              Motala i det nordvästra hörnet av Östergötland på gränsen till
-              Närke. I den lilla idylliska brunnskyrkan fick deltagarna åter
-              lyssna till en förtjusande konsert med musik och sång av unga och
-              mindre unga släktingar. Ordförandeklubban gick vidare från Hadar
-              Cars j:r till Ingrid Cars Westrell, (Gunnars barnbarn). Hon blev
-              föreningens andra ordförande från den tredje generationen Cars.
-              Detta år inrättades formellt även en funktion som hemsidesredaktör
-              och Elin Armini utsågs till detta (hon hade redan i flera år skött
-              föreningens hemsida). Det beslutades att såväl hemsidesredaktören
-              som arkivarien Kerstin Kalén skulle adjungeras till styrelsen och
-              kallas till dess möten. Middag serverades – givetvis - i
-              Societetshuset. Parentation hölls över två sedan föregående
-              släktmöte avlidna medlemmar, kusinerna Barbro Cars och Hans
-              Erfelt. En DVD med filmer från släktens historia överlämnades till
-              varje deltagare. År 2013 var det dags för det fjortonde
-              släktmötet, på Julita gård i Sörmland 24-25 augusti. Detta möte
-              var också ett jubileum, eftersom det var 25 år sedan det beslöts
-              att bilda en släktförening. Julita hade inte någon direkt koppling
-              till släkten, men till Sörmland fanns en koppling genom Eira och
-              Thyra och deras Lundtorp. Julita fungerade också mycket bra som en
-              samlingsplats för släkten. Vi hade hyrt hela Julita med alla
-              hotellrum och vandrarhemmet så vi kunde vara ifred. Det fanns
-              vidare områden där vi kunde leka och så ligger det mycket vackert
-              intill sjön Öljaren. 59 släktingar, varav en fjärdedel barn,
-              samlades på lördagseftermiddagen ute på en äng nere vid sjön för
-              kaffe och sedan olika lekar, både mer fysiska, som att kasta
-              bollar och testa olika smaker, och mer intellektuella, som
-              tipspromenad. Medlemmarna var indelade i olika lag med både barn
-              och vuxna i varje lag, som tävlade mot varandra. Tävlingarna var
-              uppskattade och vädret var vackert. Vi har ofta haft tur med
-              vädret på våra släktmöten. Även detta år kunde vi därför ha själva
-              den formella delen av mötet utomhus – på gården till Caféet, där
-              vi för övrigt åt frukost dagen därpå. Under mötet serverades ett
-              glas ”bubbel”. Hans-Christian Cars valdes till ordförande för
-              föreningen. Och förutom de stadgeenliga punkterna informerades om
-              denna Släktföreningens historia som särskilt Thorsten och Hadar
-              bidragit till. I sitt inledningsanförande nämnde vidare avgående
-              ordförande Ingrid Cars Westrell de medlemmar som avlidit sedan
-              föregående möte, Eida Cars Wesslén, Gunnar Fahlberg, Ingrid Cars
-              (som varit gift med Gunnar) och Ingemar Cars. Efter mötet
-              serverades en trerättersmiddag i Bäckstugan. Vid middagen hölls
-              några tal och bjöds på musikalisk underhållning.{" "}
+              släktföreningens styrelser.)
+              <br />
+              <br />
+              Det femte släktmötet hölls i Linköping den 19-20 augusti 1995.
+              Flera av bröderna i den ursprungliga syskonskaran hade gått i
+              skola i Linköping, två av dem – Gunnar och Thorsten – återvände
+              dit i mogen ålder med sina familjer och en hel del i den följande
+              generationen har också gått i skola där. Det var därför naturligt
+              att förlägga ett släktmöte till Linköping. Middagen avåts i den
+              vackra Mjellerumsgården i Gamla Linköping. Till ny
+              föreningsordförande efter Göran Cars valdes Thorsten Cars j:r (son
+              till Thorsten). Detta år beslöts också att utse en arkivarie för
+              att ta hand om intressanta dokument och bilder. Till arkivarie
+              utsågs Kerstin Kalén.
+              <br />
+              <br />
+              Parentation hölls över två sedan föregående släktmöte avlidna
+              medlemmar, kusinerna Barbro Cars och Hans Erfelt. En DVD med
+              filmer från släktens historia överlämnades till varje deltagare.
+              År 2013 var det dags för det fjortonde släktmötet, på Julita gård
+              i Sörmland 24-25 augusti. Detta möte var också ett jubileum,
+              eftersom det var 25 år sedan det beslöts att bilda en
+              släktförening. Julita hade inte någon direkt koppling till
+              släkten, men till Sörmland fanns en koppling genom
+              <br />
+              <br />
             </p>
           </section>
         </div>
